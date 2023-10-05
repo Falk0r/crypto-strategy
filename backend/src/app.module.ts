da5ebb30b06b15ca02efc494eb9161c75990d672
Falk0r <g.falkor@gmail.com>
2023-10-05 20:27:10 +0200

    connexion to bdd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './models/users/entities/user.entity';
+import { UsersModule } from './models/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'develop',
+      password: 'password',
+      database: 'develop',
+      entities: [User],
+      synchronize: true, // Not in production
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
