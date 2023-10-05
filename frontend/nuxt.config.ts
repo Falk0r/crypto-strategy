a2025489057eeba47e28c1c4db853bc61b143f11
Falk0r <g.falkor@gmail.com>
2023-10-05 15:33:14 +0200

    front & back connexion

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
+  srcDir: "frontend/",
+  spaLoadingTemplate: "./spa-loading-template.html",
   modules: [
     [
       '@nuxtjs/i18n',
